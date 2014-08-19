d6cacf177cd280fcd939ab74b95184640c568860
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-08-19 16:34:04 +0100

    adds listener for database changes

---
+++ config/config.js
@@ -0,0 +1,5 @@
+var config = {
+  url: 'http://stage.ebola.eocng.org:5984/sense_contacts/'
+};
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,2 +1,45 @@
 // sense-dispatch
-exports.Main = require('./lib/main');
+var config = require('./config/config');
+var PouchDB = require('pouchdb');
+var log4js = require('log4js');
+
+/*jshint camelcase: false */
+var options = {
+  live: true,
+  since: 'now',
+  include_docs: true
+};
+
+var MAX_TEMP = 38.0;
+
+var isArray = function(o) {
+  return Object.prototype.toString.call(o) === '[object Array]';
+};
+
+var getRecentVisit = function(dailyVisits) {
+  if (isArray(dailyVisits) && dailyVisits.length > 0) {
+    var sorted = dailyVisits.sort(function(a, b) {
+      return new Date(a.dateOfVisit) < new Date(b.dateOfVisit);
+    });
+    return sorted[0];
+  }
+  return;
+};
+
+var processDailyVisit = function(dv){
+  if(dv.temperature > MAX_TEMP){
+    //TODO: send sms and email here.
+  }
+};
+
+var db = new PouchDB(config.url);
+db.changes(options)
+  .on('change',function(change) {
+    var contact = change.doc;
+    var dailyVisit = getRecentVisit(contact.dailyVisits);
+    if(typeof dailyVisit !== 'undefined'){
+      processDailyVisit(dailyVisit);
+    }
+  }).on('error', function(err) {
+    console.log(err);
+  });
